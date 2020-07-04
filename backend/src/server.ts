bf7fd9b53853e66ed4f0ac0b5055141ae9087f4c
Pranjal Nadhani <pranjal@dream11.com>
2020-07-04 20:28:23 +0530

    ci: fix docker context and workdir

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import arithmeticAPI from './arithmeticAPI'
 
 const app = express()
-const port = 4000
+const port = 8081
 
 app.use(express.json())
 
--- frontend/scripts/server.js
+++ frontend/scripts/server.js
@@ -6,7 +6,7 @@ const webpack = require('webpack')
 const config = require('../webpack/dev.config')
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 
 const compiler = webpack(config)
 
